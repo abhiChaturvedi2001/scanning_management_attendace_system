9dbbd9fdbc1c3ad3f7edca974adcc83e848ea32f
ABHCH <chaturvedia435@gmail.com>
2024-02-06 15:56:14 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Correctly resolve the alias for html5-qrcode
-      'html5-qrcode': path.resolve(new URL('./node_modules/html5-qrcode', import.meta.url).pathname)
+  build: {
+    rollupOptions: {
+      external: ['html5-qrcode']
     }
   }
 });
